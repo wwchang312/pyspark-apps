5f141d240537fbf1e8f5650a75c432388e06e789
wwchang <wanw95@gmail.com>
2026-06-01 15:22:35 +0900

    테스트

--- lesson/ch12_6/json_with_list.py
+++ lesson/ch12_6/json_with_list.py
@@ -33,8 +33,8 @@ def for_each_batch_func(df: DataFrame, epoch_id):
     json_to_col_df = from_json_df.select('person_info.*') \
                                  .selectExpr('name',
                                              'address.*',
-                                             'CAST(age AS INT) AS age ',
-                                             'hobbies')
+                                             'CAST(age AS INT) AS age ') \
+                                 .select(explode('hobbies'))
     print(f'json_to_col_df.show()')
     json_to_col_df.show(truncate=False)
 
